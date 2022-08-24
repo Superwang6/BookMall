9d0f500ec3200e1737cc4dd5dd35ce2fea63eed6
Superwang6 <1075227991@qq.com>
2022-08-24 11:52:00 +0800

    测试dev分支

---
+++ server/src/main/java/com/fudges/server/test.java
@@ -0,0 +1,17 @@
+package com.fudges.server;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 王平远
+ * @since 2022/8/24
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class test {
+}
